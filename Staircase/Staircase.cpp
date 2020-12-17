8ab5eaf3d00266be9d7f754bec84869c5f886a17
Charith Dissanayake <61795948+Charith47@users.noreply.github.com>
2020-12-17 21:53:12 +0530

    Update Staircase.cpp

--- Staircase/Staircase.cpp
+++ Staircase/Staircase.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+// Splution ---
+
+void staircase(int n) {
+     for (int i = 0; i < n; i++) {
+        for (int j = n-i-1; j > 0; j--) cout << " ";
+        for (int k = 0; k <= i; k++) cout << "#";
+        cout << '\n';
+    }
+}
+// End Solution ---
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    staircase(n);
+
+    return 0;
+}
