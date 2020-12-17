f93c6f423ab8b844124ed3c254c0bbc80eee7fdd
Charith Dissanayake <61795948+Charith47@users.noreply.github.com>
2020-12-17 21:55:01 +0530

    Create Mini-Max_Sum.cpp

---
+++ Mini-Max Sum/Mini-Max_Sum.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Solution --- 
+
+void miniMaxSum(vector<int> arr) {
+    long minSum = 0;
+    long maxSum = 0;
+
+    std::sort(arr.begin(), arr.end());
+    for (unsigned int itr = 0; itr < arr.size()-1; itr++) {
+        minSum += arr[itr];
+        maxSum += arr[itr + 1];
+    }
+    cout << minSum << " " << maxSum;
+}
+
+// End Solution ---
+
+int main()
+{
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(5);
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    miniMaxSum(arr);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
